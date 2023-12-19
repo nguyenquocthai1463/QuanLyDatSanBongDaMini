c319178274dc6a1a53a14e4dc8628b9146134a18
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2023-12-20 02:29:38 +0700

    Tu
    Đăng nhập

--- QuanLyDatSanBongDaMini/TrangChu.cs
+++ QuanLyDatSanBongDaMini/TrangChu.cs
@@ -12,9 +12,37 @@ namespace QuanLyDatSanBongDaMini
 {
     public partial class TrangChu : Form
     {
+
+        string tenDangNhap = "";
+        string tenNhanVien = "";
+        string matKhau = "";
+        string quyen = "";
         public TrangChu()
         {
             InitializeComponent();
         }
+
+        public TrangChu(string tenDangNhap, string tenNhanVien, string matkhau, string quyen)
+        {
+            InitializeComponent();
+            this.tenDangNhap = tenDangNhap;
+            this.tenNhanVien = tenNhanVien;
+            this.matKhau = matkhau;
+            this.quyen = quyen;
+        }
+
+        private void form_quanlynhanvien_Click(object sender, EventArgs e)
+        {
+            if (quyen == "quanly")
+            {
+                QuanLyNhanVien f = new QuanLyNhanVien();
+                f.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Bạn không có quyền truy cập chức năng!", "Thông báo !", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
     }
 }
